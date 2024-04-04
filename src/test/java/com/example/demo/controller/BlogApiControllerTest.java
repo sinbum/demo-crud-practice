cfafec3eddc47c7f946281bba75e2d73e699ef12
sinbum <sinbum@kakao.com>
2024-04-04 13:07:53 +0900

    테스트 : 브로그 글 목록 조회 성공

--- src/test/java/com/example/demo/controller/BlogApiControllerTest.java
+++ src/test/java/com/example/demo/controller/BlogApiControllerTest.java
@@ -20,7 +20,9 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -90,6 +92,41 @@ class BlogApiControllerTest {
 
 //        내용 검증
         assertThat(articleList.get(0).getContent()).isEqualTo(content); //왼쪽 조건의 연ㅅ나이, 오른쪽 값과 같은지.
+
+    }
+
+
+    @DisplayName("findAllArticles: 블로그 글 목록 조회에 성공")
+    @Test
+    public void findAllArticles() throws Exception {
+        //        given
+        final String url = "/api/aritcles";
+        final String title = "title";
+        final String content = "content";
+
+        blogRepository.save(
+                Article.builder()
+                        .title(title)
+                        .content(content)
+                        .build()
+
+        );
+
+        //        when
+        // 수행하는 로직
+        final ResultActions resultActions = mockMvc
+                .perform(
+                        get(url)
+                                .accept(MediaType.APPLICATION_JSON)
+                );
+
+
+        //        then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].title").value(title))
+                .andExpect(jsonPath("$[0].content").value(content));
+
 
     }
 
