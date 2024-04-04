712032c3cdf5dcf30ef729063e6ce52de76f6ef9
sinbum <sinbum@kakao.com>
2024-04-04 13:07:11 +0900

    아티클 목록조회

--- src/main/java/com/example/demo/controller/BlogApiController.java
+++ src/main/java/com/example/demo/controller/BlogApiController.java
@@ -2,13 +2,17 @@ package com.example.demo.controller;
 
 import com.example.demo.domain.Article;
 import com.example.demo.dto.AddArticleRequest;
+import com.example.demo.dto.ArticleResponse;
 import com.example.demo.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,6 +27,16 @@ public class BlogApiController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/aritcles")
+    public ResponseEntity<List<ArticleResponse>> findAllAritcles(){
+        List<ArticleResponse> articles = blogService.finaAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok().body(articles);
     }
 
 }
--- src/main/java/com/example/demo/service/BlogService.java
+++ src/main/java/com/example/demo/service/BlogService.java
@@ -6,6 +6,8 @@ import com.example.demo.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class BlogService {
@@ -14,6 +16,10 @@ public class BlogService {
 
     public Article save(AddArticleRequest request){
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> finaAll(){
+        return blogRepository.findAll();
     }
 
 
