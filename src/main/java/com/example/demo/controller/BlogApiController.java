2035d6f7e79c49d728d976f7b503f62b8c1e63f6
sinbum <sinbum@kakao.com>
2024-04-04 13:20:54 +0900

    블로그 글 단일 조회

--- src/main/java/com/example/demo/controller/BlogApiController.java
+++ src/main/java/com/example/demo/controller/BlogApiController.java
@@ -7,10 +7,7 @@ import com.example.demo.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,7 +18,7 @@ public class BlogApiController {
     private final BlogService blogService;
 
     @PostMapping("/api/articles")
-    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
         Article savedArticle = blogService.save(request);
 
         return ResponseEntity
@@ -30,13 +27,19 @@ public class BlogApiController {
     }
 
     @GetMapping("/api/aritcles")
-    public ResponseEntity<List<ArticleResponse>> findAllAritcles(){
+    public ResponseEntity<List<ArticleResponse>> findAllAritcles() {
         List<ArticleResponse> articles = blogService.finaAll()
                 .stream()
                 .map(ArticleResponse::new)
                 .toList();
 
         return ResponseEntity.ok().body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable("id") long id) {
+        Article article = blogService.findById(id);
+        return ResponseEntity.ok().body(new ArticleResponse(article));
     }
 
 }
--- src/main/java/com/example/demo/service/BlogService.java
+++ src/main/java/com/example/demo/service/BlogService.java
@@ -22,5 +22,12 @@ public class BlogService {
         return blogRepository.findAll();
     }
 
+    public Article findById(long id){
+        return blogRepository.findById(id)
+                .orElseThrow(
+                        () -> new IllegalArgumentException("not found : " + id)
+                );
+    }
+
 
 }
